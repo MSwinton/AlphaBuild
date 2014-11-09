d1a1f21118b71ce4d3747de60251058a80051af9
benjaminpstern <bstern@oberlin.edu>
2014-11-09 01:12:35 -0500

    added a sound effect

--- Assets/Scripts/PushMine.cs
+++ Assets/Scripts/PushMine.cs
@@ -2,13 +2,18 @@
 using System.Collections;
 
 public class PushMine : Mine {
+	GameObject audioObject;
 	public float magnitude = 15;
+	void Start(){
+		audioObject = Resources.Load<GameObject>("Prefabs/Boing");
+	}
 	public void init(GameObject blowupables, float armTime,float detRad, float blastRad, float mag){
 		base.init(blowupables, armTime, detRad, blastRad);
 		this.magnitude = mag;
 	}
 	public override void activate(Splodeable s){
 		Vector3 r = s.transform.position - this.transform.position;
+		Instantiate(audioObject);
 		if(r.magnitude > 0)
 			s.push(r.normalized * magnitude / r.magnitude);
 		else{
---
+++ Assets/Scripts/destroySelf.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroySelf : MonoBehaviour {
+	public float timer;
+	void Update () {
+		timer -= Time.deltaTime;
+		if(timer <= 0){
+			Destroy(gameObject);
+		}
+	}
+}
