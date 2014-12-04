736022f45a208251a778ad096622cb78548a69e9
Shiying <szheng@oberlin.edu>
2014-12-04 03:07:54 -0500

    Home menu and custom mine bar

---
+++ Assets/Scripts/HomeMenuController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomeMenuController : MonoBehaviour {
+
+	public GUIStyle startButtonStyle;
+	public GUIStyle tutorialButtonStyle;
+
+	public Texture gameName;
+
+	void OnGUI(){
+		GUI.Label(new Rect(Screen.width / 2 - Screen.width / 4, Screen.height / 4 - Screen.height / 6 , Screen.width / 2, Screen.height / 3 ), gameName);
+
+		if (GUI.Button(new Rect(Screen.width / 2 - Screen.width / 6, Screen.height / 2 - Screen.height / 8 , Screen.width / 3, Screen.height / 4 ),"", 
+		               startButtonStyle)){
+			
+		}
+		if (GUI.Button(new Rect(Screen.width / 2 - Screen.width / 6, Screen.height / 4 * 3 - Screen.height / 16 , Screen.width / 3, Screen.height / 4),"", 
+		               tutorialButtonStyle)){
+			
+		}
+	}
+}
