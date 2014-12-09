d3d366816c80d35dbfafb04ddcce30961ad9e223
Shiying <szheng@oberlin.edu>
2014-12-08 21:56:46 -0500

    Light tower flashes red when player enters

--- Assets/Scripts/SentinelTower.cs
+++ Assets/Scripts/SentinelTower.cs
@@ -2,6 +2,12 @@
 using System.Collections;
 
 public class SentinelTower : Enemy {
+	GameObject light;
+	Color lightColor;
+	Color inactiveColor;
+	Color activeColor;
+	float blinkTimer;
+	bool blink;
 
 	void Start () {
 		base.init();
@@ -10,18 +16,46 @@ public class SentinelTower : Enemy {
 		destLocation = this.transform.position;
 		player = GameObject.FindGameObjectWithTag ("Player");
 		playerInvisibility = player.GetComponent<Invisibility>();
+		light = transform.FindChild("Area Light").gameObject;
+		inactiveColor = Color.blue;
+		activeColor = Color.red;
+		blinkTimer = 0.1f;
+		blink = false;
 	}
 	
 	public void OnTriggerEnter2D( Collider2D other ){
 		if( other.tag == "Player" ){
 			playerInvisibility.canInvis = false;
 			playerInvisibility.turnVisible();
+			light.light.color = activeColor;
+			blink = true;
 		}
 	}
 	
 	public void OnTriggerExit2D( Collider2D other ){
-		if( other.tag == "Player" )
+		if( other.tag == "Player" ){
 			playerInvisibility.canInvis = true;
+			light.light.color = inactiveColor;
+			blink = false;
+			light.light.intensity = 2.5f;
+		}
+	}
+
+	void Update(){
+		float t = Time.deltaTime;
+		if (blink){
+			if (blinkTimer >= 0){
+				blinkTimer -= t;
+				light.light.intensity = 2.5f;
+			}
+			else if (blinkTimer >= -0.1f){
+				blinkTimer -= t;
+				light.light.intensity = 1.0f;
+			}
+			else{
+				blinkTimer = 0.1f;
+			}
+		}
 	}
 	
 	public override void explode(){
