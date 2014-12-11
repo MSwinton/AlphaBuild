c79378eebc54c9bfc1023ceef4b5af303b95a103
Shiying <szheng@oberlin.edu>
2014-12-11 10:19:23 -0500

    No picking up helmets when you already have one on

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -16,34 +16,51 @@ public class Collectable : MonoBehaviour {
 	public string thingName;
 	public int amount = 3;
 	GameObject audioObject;
+	GameObject cameraObject;
+	Spawn_GUI gui;
 	void Start(){
 		audioObject = Resources.Load<GameObject>("Prefabs/Bling");
+		cameraObject = GameObject.FindGameObjectWithTag("MainCamera");
+		gui = cameraObject.GetComponent<Spawn_GUI>();
 	}
 	void OnTriggerEnter2D( Collider2D other ){
 		if( other.tag == "Player" ){ 
-			Instantiate(audioObject);
-			GameObject cameraObject = GameObject.FindGameObjectWithTag("MainCamera");
-			Spawn_GUI gui = cameraObject.GetComponent<Spawn_GUI>();
 			if( thingName.ToLower() == "flash" ){
+				Instantiate(audioObject);
 				other.GetComponent<PlaceFlashMine>().numMines += amount;
+				Destroy(gameObject);
+				return;
 			}
 			if( thingName.ToLower() == "blowup" ){
+				Instantiate(audioObject);
 				other.GetComponent<PlaceBlowupMine>().numMines += amount;
 				gui.blowupBlingTimer = 1.0f;
+				Destroy(gameObject);
+				return;
 			}
 			if( thingName.ToLower() == "slow" ){
+				Instantiate(audioObject);
 				other.GetComponent<PlaceSlowMine>().numMines += amount;
 				gui.slowBlingTimer = 1.0f;
+				Destroy(gameObject);
+				return;
 			}
 			if( thingName.ToLower() == "push" ){
+				Instantiate(audioObject);
 				other.GetComponent<PlacePushMine>().numMines += amount;
 				gui.pushBlingTimer = 1.0f;
+				Destroy(gameObject);
+				return;
 			}
 			if( thingName.ToLower() == "invisijuice" ){
+				Instantiate(audioObject);
 				other.GetComponent<Invisibility>().invisijuice += amount;
 				gui.invisBlingTimer = 1.0f;
+				Destroy(gameObject);
+				return;
 			}
-			if( thingName.ToLower() == "shield" ){
+			if( thingName.ToLower() == "shield" && other.GetComponent<Movement>().shielded == false){
+				Instantiate(audioObject);
 				GameObject helmetObj = GameObject.FindGameObjectWithTag("Helmet");
 				if(helmetObj != null){
 					Destroy(helmetObj);
@@ -53,8 +70,9 @@ public class Collectable : MonoBehaviour {
 				GameObject helmObj = Instantiate(Resources.Load ("Prefabs/Helmet"),Vector3.zero,Quaternion.identity) as GameObject;
 				helmObj.transform.parent = other.transform;
 				helmObj.transform.localPosition = new Vector3(0,0,-.1f);
+				Destroy(gameObject);
+				return;
 			}
-			Destroy(gameObject);
 		}
 	}
 	
